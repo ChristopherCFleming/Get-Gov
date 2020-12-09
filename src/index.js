dd3c0f0c08669e694f5ee75b86a08b2cf49fb27d
ChristopherCFleming <chris.charnquist@gmail.com>
2020-12-09 14:45:34 -0500

    Use the sample bar chart

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 //import all style sheets here!
 import "./styles/reset.scss"
 import "./styles/homepage.scss"
--- src/scripts/barchart_general.js
+++ src/scripts/barchart_general.js
@@ -4,13 +4,19 @@ class BarChart extends Component {
   }
     
   drawChart() {
-    const data = [12, 5, 6, 6, 9, 10];
+    const data = this.props.data;
     
-    const svg = d3.select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .style("margin-left", 100);
+    const svg = d3.select("sample").append("svg")    //Where we connect the SVG to our HTML? 
+    .attr("width", this.props.width)
+    .attr("height", this.props.height);
+
+    svg.selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text((d) => d)
+    .attr("x", (d, i) => i * 70)
+    .attr("y", (d, i) => h - (10 * d) - 3)
                   
     svg.selectAll("rect")
       .data(data)
